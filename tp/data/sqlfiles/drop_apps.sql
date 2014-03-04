29e07b04a1c7e57fbe41a2767d458ca3d8362ca7
Allen Sanabria <asanabria@linuxdynasty.org>
2014-03-04 11:06:09 -0800

    removed unnecessary files and folders

--- tp/data/sqlfiles/drop_apps.sql
+++
@@ -1,6 +0,0 @@
-drop table refs;
-drop table cvss;
-drop table cves;
-drop table versions;
-drop table products;
-drop table vendors;
--- tp/test/test_comparer.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-
-from comparer.base import Comparer
-
-c = Comparer()
-
-apps = c.run(Comparer.NODE, date.today())
