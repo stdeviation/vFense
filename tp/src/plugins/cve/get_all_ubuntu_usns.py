8a4511a986df4b2f1fcb4761ae2ab66677410ef9
Allen Sanabria <asanabria@linuxdynasty.org>
2014-03-25 12:25:09 -0700

    I had to add some try catch exceptions, since it looks like the ubuntu usn page is starting to rate limit the parsing.

--- tp/src/plugins/cve/get_all_ubuntu_usns.py
+++ tp/src/plugins/cve/get_all_ubuntu_usns.py
@@ -1,4 +1,5 @@
 from BeautifulSoup import BeautifulSoup
+from time import sleep
 import requests
 import re
 import sys
@@ -153,8 +154,12 @@ def get_details(soup_details):
 
 def write_content_to_file(file_location, url):
     usn_file = open(file_location, 'wb')
-    usn_page = requests.get(url)
-    usn_page.close()
+    try:
+        #print write_content_to_file.func_name, url
+        usn_page = requests.get(url)
+        usn_page.close()
+    except Exception as e:
+        write_content_to_file(file_location,url)
     completed = False
     content = None
     if usn_page.ok:
@@ -167,28 +172,38 @@ def write_content_to_file(file_location, url):
 
 
 def get_url_content(usn_uri):
+    def get_usn_uri(usn_uri):
+        try:
+            usn_page = requests.get(usn_uri, timeout=1)
+            usn_page.close()
+            if usn_page.ok:
+                content = usn_page.content
+                completed = True
+
+        except Exception as e:
+            get_usn_uri(usn_uri)
+            completed = False,
+            content = None
+
+        return(completed, content)
+
     content = None
     completed = False
     usn = usn_uri.split('/')[-2]
-    if re.search('http', usn_uri):
-        usn_page = requests.get(usn_uri)
-        usn_page.close()
-        if usn_page.ok:
-            content = usn_page.content
-            completed = True
-    else:
-        usn_file_location = HTML_DIR_UBUNTU + usn
-        if not os.path.exists(usn_file_location):
-            content, completed = (
-                write_content_to_file(
-                    usn_file_location, MAIN_URL + usn_uri
-                )
-            )
-        elif os.stat(usn_file_location).st_size > 0:
+    usn_file_location = HTML_DIR_UBUNTU + usn
+    if os.path.exists(usn_file_location):
+        if os.stat(usn_file_location).st_size > 0:
             content = open(usn_file_location, 'r').read()
             completed = True
-
         else:
+            if re.search('http', usn_uri):
+                #print get_url_content.func_name, usn_uri
+                completed, content = get_usn_uri(usn_uri)
+
+    elif not os.path.exists(usn_file_location):
+        if re.search('http', usn_uri):
+            #print get_url_content.func_name, usn_uri
+            completed, content = get_usn_uri(usn_uri)
             content, completed = (
                 write_content_to_file(
                     usn_file_location, MAIN_URL + usn_uri
@@ -247,55 +262,64 @@ def process_usn_page(usn_uri):
 
 
 def begin_usn_home_page_processing(next_page=None, full_parse=False):
-    logger.info('starting ubuntu usn update process')
     if next_page:
         url = MAIN_USN_URL + '/' + next_page
-        main_page = requests.get(url)
+        #print begin_usn_home_page_processing.func_name, url
+        try:
+            main_page = requests.get(url, timeout=1)
+        except Exception as e:
+            return(begin_usn_home_page_processing(next_page, True))
     else:
-        main_page = requests.get(MAIN_USN_URL)
+        #print begin_usn_home_page_processing.func_name, MAIN_USN_URL
+        try:
+            main_page = requests.get(MAIN_USN_URL)
+        except Exception as e:
+            return(begin_usn_home_page_processing(full_parse=True))
 
-    if main_page.ok:
-        soup = BeautifulSoup(main_page.content)
-        main_page.close()
-        next_page = (
-            soup.find(
-                'div',
-                { 
-                    'class': 'pagination'
-                }
-            ).find(
-                'a',
-                {
-                    'href': re.compile(NEXT_PAGE)
-                },
-                text=re.compile('Next'),
+    try:
+        if main_page.ok:
+            soup = BeautifulSoup(main_page.content)
+            main_page.close()
+            next_page = (
+                soup.find(
+                    'div',
+                    { 
+                        'class': 'pagination'
+                    }
+                ).find(
+                    'a',
+                    {
+                        'href': re.compile(NEXT_PAGE)
+                    },
+                    text=re.compile('Next'),
+                )
             )
-        )
-        usn_uris = (
-            soup.find(
-                'div',
-                {
-                    'id': 'content'
-                }
-            ).findAll(
-                'a',
-                {
-                    'href': re.compile(USR_URI)
-                }
+            usn_uris = (
+                soup.find(
+                    'div',
+                    {
+                        'id': 'content'
+                    }
+                ).findAll(
+                    'a',
+                    {
+                        'href': re.compile(USR_URI)
+                    }
+                )
             )
-        )
-        if len(usn_uris) > 0:
-            data = []
-            for usn_uri in usn_uris:
-                data_to_update, ok = process_usn_page(usn_uri['href'])
-                if ok:
-                    data = data + data_to_update
-            insert_into_bulletin_collection_for_ubuntu(data)
+            if len(usn_uris) > 0:
+                data = []
+                for usn_uri in usn_uris:
+                    data_to_update, ok = process_usn_page(usn_uri['href'])
+                    if ok:
+                        data = data + data_to_update
+                insert_into_bulletin_collection_for_ubuntu(data)
 
-        if full_parse:
-            if next_page:
-                begin_usn_home_page_processing(next_page.parent['href'], True)
+            if full_parse:
+                if next_page:
+                    return(begin_usn_home_page_processing(next_page.parent['href'], True))
+
+    except Exception as e:
+        logger.exception(e)
 
     logger.info('finished ubuntu usn update process')
-
-
