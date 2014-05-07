b24a4d5da27ad65afe2736f42ab4a05f558f7478
Humberto Chacon <humchacon@gmail.com>
2014-05-07 16:23:50 -0400

    Moving things around. Writing placeholder code.

---
+++ tp/src/plugins/patching/vFense_apps/agent_app_update.py
@@ -0,0 +1,5 @@
+
+def add_available_agent_update_to_agent(
+    username, agent_id, customer_name, app_data
+    ):
+    pass
---
+++ tp/src/receiver/api/rv/agent_update.py
@@ -0,0 +1,42 @@
+import logging
+
+from vFense.core.api.base import BaseHandler
+from vFense.core.decorators import agent_authenticated_request, \
+    convert_json_to_arguments
+from vFense.core.user.users import get_user_property
+from vFense.core.user import UserKeys
+from vFense.receiver.rvhandler import RvHandOff
+
+from vFense.operations._constants import AgentOperations
+
+logging.config.fileConfig('/opt/TopPatch/conf/logging.config')
+logger = logging.getLogger('rvlistener')
+
+
+class AgentUpdateHandler(BaseHandler):
+    @agent_authenticated_request
+    @convert_json_to_arguments
+    def post(self, agent_id):
+        username = self.get_current_user()
+        customer_name = (
+            get_user_property(username, UserKeys.CurrentCustomer)
+        )
+
+        uri = self.request.uri
+        method = self.request.method
+
+        try:
+            operation_id = self.arguments.get('operation_id', None)
+            error = self.arguments.get('error', None)
+            success = self.arguments.get('success', 'true')
+            app_data = self.arguments.get('data')
+            status_code = self.arguments.get('status_code', None)
+
+            RvHandOff(
+               username, customer_name, uri, method, agent_id,
+               app_data, oper_type=AgentOperations.AVAILABLE_AGENT_UPDATE
+            )
+
+        except Exception as e:
+            logger.exception(e)
+
