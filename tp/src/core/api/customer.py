86fe9c289eed4eab7c4fb9c1a487f0447c150014
Humberto Chacon <humchacon@gmail.com>
2014-05-17 11:50:44 -0400

    Moving new Customer class to appropriate file.

--- tp/src/core/api/customer.py
+++ tp/src/core/api/customer.py
@@ -18,7 +18,7 @@ from vFense.core.agent.agents import change_customer_for_all_agents_in_customer,
     remove_all_agents_for_customer
 
 from vFense.core.user import *
-from vFense.core.customer import CustomerKeys
+from vFense.core.customer import Customer, CustomerKeys
 
 from vFense.core.customer.customers import get_properties_for_customer, \
     get_properties_for_all_customers, get_customer, remove_customer, \
@@ -36,110 +36,6 @@ from vFense.plugins.patching.patching import remove_all_apps_for_customer, \
 
 logging.config.fileConfig('/opt/TopPatch/conf/logging.config')
 logger = logging.getLogger('rvapi')
-
-
-class Customer(object):
-    """Used to represent an instance of a customer."""
-
-    def __init__(
-        self, name, net_throttle=None, cpu_throttle=None,
-        server_queue_ttl=None, agent_queue_ttl=None,
-        package_download_url=None
-    ):
-        self.name = name
-        self.net_throttle = net_throttle
-        self.cpu_throttle = cpu_throttle
-        self.server_queue_ttl = server_queue_ttl
-        self.agent_queue_ttl = agent_queue_ttl
-        self.package_download_url = package_download_url
-
-        if net_throttle:
-            self.net_throttle = int(net_throttle)
-
-        if server_queue_ttl:
-            self.server_queue_ttl = int(server_queue_ttl)
-
-        if agent_queue_ttl:
-            self.agent_queue_ttl = int(agent_queue_ttl)
-
-    def get_invalid_fields(self):
-        """Check the customer for any invalid fields.
-        
-        Returns:
-            (list): List of key/value pair dictionaries corresponding
-                to the invalid fields.
-        """
-        invalid_fields = []
-
-        if not isinstance(self.name, basestring):
-            invalid_fields.append(
-                {CustomerKeys.CustomerName: self.name}
-            )
-
-        if self.net_throttle:
-            if isinstance(self.net_throttle, int):
-                if self.net_throttle < 0:
-                    invalid_fields.append(
-                        {CustomerKeys.NetThrottle: self.net_throttle}
-                    )
-            else:
-                invalid_fields.append(
-                    {CustomerKeys.NetThrottle: self.net_throttle}
-                )
-
-        if self.cpu_throttle:
-            if self.cpu_throttle not in CPUThrottleValues.VALID_VALUES:
-                invalid_fields.append(
-                    {CustomerKeys.CpuThrottle: self.cpu_throttle}
-                )
-
-        if self.server_queue_ttl:
-            if isinstance(self.server_queue_ttl, int):
-                if self.server_queue_ttl <= 0:
-                    invalid_fields.append(
-                        {CustomerKeys.ServerQueueTTL: self.server_queue_ttl}
-                    )
-            else:
-                invalid_fields.append(
-                    {CustomerKeys.ServerQueueTTL: self.server_queue_ttl}
-                )
-
-        if self.agent_queue_ttl:
-            if isinstance(self.agent_queue_ttl, int):
-                if self.agent_queue_ttl <= 0:
-                    invalid_fields.append(
-                        {CustomerKeys.AgentQueueTTL: self.agent_queue_ttl}
-                    )
-        else:
-            invalid_fields.append(
-                {CustomerKeys.AgentQueueTTL: self.agent_queue_ttl}
-            )
-
-        # TODO: check for invalid package url
-
-        return invalid_fields
-
-    def to_dict(self):
-        return {
-            CustomerKeys.CustomerName: self.name,
-            CustomerKeys.NetThrottle: self.net_throttle,
-            CustomerKeys.CpuThrottle: self.cpu_throttle,
-            CustomerKeys.ServerQueueTTL: self.server_queue_ttl,
-            CustomerKeys.AgentQueueTTL: self.agent_queue_ttl,
-            CustomerKeys.PackageUrl: self.package_download_url
-        }
-
-    def to_dict_non_null(self):
-        """ Use to get non None attributes of customer. Useful when
-        filling out just a few fields to update the customer in the db.
-
-        Returns:
-            (dict): a dictionary with non None items.
-        """
-        customer_dict = self.to_dict()
-
-        return {k:customer_dict[k] for k in customer_dict
-                if customer_dict[k] != None}
 
 
 class CustomerHandler(BaseHandler):
--- tp/src/core/customer/__init__.py
+++ tp/src/core/customer/__init__.py
@@ -1,3 +1,5 @@
+from vFense.core._constants import CPUThrottleValues
+
 class CustomerCollections():
     Customers = 'customers'
     CustomersPerUser = 'customers_per_user'
@@ -24,3 +26,109 @@ class CustomerPerUserKeys():
 class CustomerPerUserIndexes():
     CustomerName = 'customer_name'
     UserName = 'user_name'
+
+
+class Customer(object):
+    """Used to represent an instance of a customer."""
+
+    def __init__(
+        self, name, net_throttle=None, cpu_throttle=None,
+        server_queue_ttl=None, agent_queue_ttl=None,
+        package_download_url=None
+    ):
+        self.name = name
+        self.net_throttle = net_throttle
+        self.cpu_throttle = cpu_throttle
+        self.server_queue_ttl = server_queue_ttl
+        self.agent_queue_ttl = agent_queue_ttl
+        self.package_download_url = package_download_url
+
+        if net_throttle:
+            self.net_throttle = int(net_throttle)
+
+        if server_queue_ttl:
+            self.server_queue_ttl = int(server_queue_ttl)
+
+        if agent_queue_ttl:
+            self.agent_queue_ttl = int(agent_queue_ttl)
+
+    def get_invalid_fields(self):
+        """Check the customer for any invalid fields.
+        
+        Returns:
+            (list): List of key/value pair dictionaries corresponding
+                to the invalid fields.
+        """
+        invalid_fields = []
+
+        if not isinstance(self.name, basestring):
+            invalid_fields.append(
+                {CustomerKeys.CustomerName: self.name}
+            )
+
+        if self.net_throttle:
+            if isinstance(self.net_throttle, int):
+                if self.net_throttle < 0:
+                    invalid_fields.append(
+                        {CustomerKeys.NetThrottle: self.net_throttle}
+                    )
+            else:
+                invalid_fields.append(
+                    {CustomerKeys.NetThrottle: self.net_throttle}
+                )
+
+        if self.cpu_throttle:
+            if self.cpu_throttle not in CPUThrottleValues.VALID_VALUES:
+                invalid_fields.append(
+                    {CustomerKeys.CpuThrottle: self.cpu_throttle}
+                )
+
+        if self.server_queue_ttl:
+            if isinstance(self.server_queue_ttl, int):
+                if self.server_queue_ttl <= 0:
+                    invalid_fields.append(
+                        {CustomerKeys.ServerQueueTTL: self.server_queue_ttl}
+                    )
+            else:
+                invalid_fields.append(
+                    {CustomerKeys.ServerQueueTTL: self.server_queue_ttl}
+                )
+
+        if self.agent_queue_ttl:
+            if isinstance(self.agent_queue_ttl, int):
+                if self.agent_queue_ttl <= 0:
+                    invalid_fields.append(
+                        {CustomerKeys.AgentQueueTTL: self.agent_queue_ttl}
+                    )
+            else:
+                invalid_fields.append(
+                    {CustomerKeys.AgentQueueTTL: self.agent_queue_ttl}
+                )
+
+        # TODO: check for invalid package url
+
+        return invalid_fields
+
+    def to_dict(self):
+        return {
+            CustomerKeys.CustomerName: self.name,
+            CustomerKeys.NetThrottle: self.net_throttle,
+            CustomerKeys.CpuThrottle: self.cpu_throttle,
+            CustomerKeys.ServerQueueTTL: self.server_queue_ttl,
+            CustomerKeys.AgentQueueTTL: self.agent_queue_ttl,
+            CustomerKeys.PackageUrl: self.package_download_url
+        }
+
+    def to_dict_non_null(self):
+        """ Use to get non None attributes of customer. Useful when
+        filling out just a few fields to update the customer in the db.
+
+        Returns:
+            (dict): a dictionary with non None items.
+        """
+        customer_dict = self.to_dict()
+
+        return {k:customer_dict[k] for k in customer_dict
+                if customer_dict[k] != None}
+
+
