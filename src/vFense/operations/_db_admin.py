45a6dcad8fa64fbd6b0861adeac24ee8124e13ff
Allen Sanabria <asanabria@linuxdynasty.org>
2014-06-09 17:45:16 -0400

    Begun the admin search db calls

--- src/vFense/operations/_db_admin.py
+++ src/vFense/operations/_db_admin.py
@@ -1,6 +1,48 @@
+import logging, logging.config
+
+from vFense import VFENSE_LOGGING_CONFIG
+from vFense.db.client import db_create_close, r
+from vFense.core._db import insert_data_in_table
+
 from vFense.operations._db_model import (
     OperationCollections, AdminOperationKey, AdminOperationIndexes
 )
 
+logging.config.fileConfig(VFENSE_LOGGING_CONFIG)
+logger = logging.getLogger('rvapi')
+
+@db_create_close
+def fetch_admin_operation(operation_id, conn=None):
+    """Fetch an operation by id and all of it's information
+    Args:
+        operation_id (str): 36 character UUID
+
+    Basic Usage:
+        >>> from vFense.operations._db_admin import fetch_admin_operation
+        >>> operation_id = '8fed3dc7-33d4-4278-9bd4-398a68bf7f22'
+        >>> fetch_admin_operation(operation_id)
+
+    Returns:
+        Dictionary
+    """
+    data = {}
+    try:
+        data = (
+            r
+            .table(OperationCollections.Admin)
+            .get(operation_id)
+            .run(conn)
+        )
+
+    except Exception as e:
+        logger.exception(e)
+
+    return data
 
 
+def insert_admin_operation(operation_data):
+    results = (
+        insert_data_in_table(operation_data, OperationCollections.Admin)
+    )
+
+    return results
---
+++ src/vFense/operations/search/_db_admin_search.py
@@ -0,0 +1,118 @@
+import logging, logging.config
+
+from vFense import VFENSE_LOGGING_CONFIG
+from vFense.db.client import db_create_close, r
+from vFense.core._db import insert_data_in_table
+from vFense.operations.search._constants import OperationSearchValues
+from vFense.core._constants import SortValues, DefaultQueryValues
+
+from vFense.operations._db_model import (
+    OperationCollections, AdminOperationKey, AdminOperationIndexes
+)
+
+logging.config.fileConfig(VFENSE_LOGGING_CONFIG)
+logger = logging.getLogger('rvapi')
+
+class FetchAdminOperations(object):
+    """Agent operation database queries"""
+    def __init__(
+            self, view_name=None,
+            count=DefaultQueryValues.COUNT,
+            offset=DefaultQueryValues.OFFSET,
+            sort=SortValues.ASC,
+            sort_key=AdminOperationKey.CreatedTime
+        ):
+        """
+        Kwargs:
+            view_name (str): Name of the current view.
+                default = None
+            count (int): Maximum number of results to return.
+                default = 30
+            offset (int): Retrieve operations after this number. Pagination.
+                default = 0
+            sort (str): Sort either by asc or desc.
+                default = desc
+            sort_key (str): Sort by a valid field.
+                examples... operation, status, created_time, updated_time,
+                completed_time, and created_by.
+                default = created_time
+
+        Basic Usage:
+            >>> from vFense.operations.search._db_agent_search import FetchAgentOperations
+            >>> view_name = 'default'
+            >>> operation = FetchAgentOperations(view_name)
+        """
+
+        self.view_name = view_name
+        self.count = count
+        self.offset = offset
+        self.sort_key = sort_key
+
+        if sort == SortValues.ASC:
+            self.sort = r.asc
+        else:
+            self.sort = r.desc
+
+    @db_create_close
+    def fetch_all(self, conn=None):
+        """Fetch all operations
+        Basic Usage:
+            >>> from vFense.operations.search._db_admin_search import FetchAdminOperations
+            >>> view_name = 'global'
+            >>> operation = FetchAgentOperations(view_name)
+            >>> operation.fetch_all()
+        Returns:
+            List
+        """
+        count = 0
+        data = []
+        base_filter = self._set_admin_operation_base_query()
+        base_time_merge = self._set_base_time_merge()
+        try:
+            count = (
+                base_filter
+                .count()
+                .run(conn)
+            )
+
+            data = list(
+                base_filter
+                .order_by(self.sort(self.sort_key))
+                .skip(self.offset)
+                .limit(self.count)
+                .merge(base_time_merge)
+                .run(conn)
+            )
+
+        except Exception as e:
+            logger.exception(e)
+
+        return(count, data)
+
+    def _set_admin_operation_base_query(self):
+        base_filter = (
+            r
+            .table(OperationCollections.Admin)
+        )
+        if self.view_name:
+            base_filter = (
+                r
+                .table(OperationCollections.Admin)
+                .get_all(
+                    self.view_name,
+                    index=AdminOperationIndexes.CurrentView
+                )
+            )
+
+        return base_filter
+
+    def _set_base_time_merge(self):
+        merge = (
+            {
+                AdminOperationKey.CreatedTime: (
+                    r.row[AdminOperationKey.CreatedTime].to_epoch_time()
+                ),
+            }
+        )
+
+        return merge
