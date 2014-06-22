cc2d1adf7e5f93bedc99d7a7e0edea245942d72b
Allen Sanabria <asanabria@linuxdynasty.org>
2014-06-22 15:49:55 -0400

    Moved ubuntu db model into its own file and starting to work on search for ubuntu vulnerabilities

---
+++ src/vFense/plugins/vuln/ubuntu/_db_model.py
@@ -0,0 +1,18 @@
+class UbuntuSecurityCollection():
+    Bulletin = 'ubuntu_security_bulletin'
+
+
+class UbuntuSecurityBulletinKey():
+    DatePosted = 'date_posted'
+    BulletinId = 'bulletin_id'
+    Summary = 'bulletin_summary'
+    Details = 'bulletin_details'
+    Apps = 'apps'
+    OsStrings = 'os_strings'
+    CveIds = 'cve_ids'
+    SupportUrl = 'support_url'
+
+class UbuntuSecurityBulletinIndexes():
+    BulletinId = 'bulletin_id'
+    NameAndVersion = 'name_and_version'
+    CveIds = 'cve_ids'
