2522506cd0838ec2ed29274c89d667d987db776b
Allen Sanabria <asanabria@linuxdynasty.org>
2014-06-28 14:27:35 -0400

    api arguments for apps

---
+++ src/vFense/plugins/patching/api/_constants.py
@@ -0,0 +1,14 @@
+class AppApiArguments():
+    STATUS = 'status'
+    SEVERITY = 'severity'
+    VULN = 'vuln'
+    HIDDEN = 'hidden'
+    APP_IDS = 'app_ids'
+    TIME = 'time'
+    LABEL = 'label'
+    RESTART = 'restart'
+    CPU_THROTTLE = 'cpu_throttle'
+    NET_THROTTLE = 'net_throttle'
+
+class AppFilterValues():
+    SEVERITY = 'severity'
