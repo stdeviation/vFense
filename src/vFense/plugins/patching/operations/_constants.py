4fdc22acd3842a21fe1d3f4b83bb38004207bbd6
Allen Sanabria <asanabria@linuxdynasty.org>
2014-07-13 20:58:19 -0400

    InstallKeys

---
+++ src/vFense/plugins/patching/operations/_constants.py
@@ -0,0 +1,8 @@
+class InstallKeys():
+    AGENT_IDS = 'agent_ids'
+    APP_IDS = 'app_ids'
+    NET_THROTTLE = 'net_throttle'
+    CPU_THROTTLE = 'cpu_throttle'
+    RESTART = 'restart'
+    USER_NAME = 'user_name'
+    VIEW_NAME = 'view_name'
