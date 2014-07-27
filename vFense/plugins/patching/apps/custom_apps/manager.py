d0a6ba59d7637f829ce6090425787224b80a72d7
Allen Sanabria <asanabria@linuxdynasty.org>
2014-07-27 00:19:08 -0400

    Still working on custom apps

--- vFense/plugins/patching/apps/custom_apps/manager.py
+++ vFense/plugins/patching/apps/custom_apps/manager.py
@@ -3,6 +3,7 @@ import logging
 from vFense import VFENSE_LOGGING_CONFIG
 from vFense.core.agent._db_model import *
 from vFense.db.client import r
+from vFense.plugins.patching import AppsPerAgent, Files
 from vFense.plugins.patching._db_model import *
 from vFense.plugins.patching._constants import CommonAppKeys
 from vFense.core.agent._db import(
@@ -24,13 +25,19 @@ class CustomAppsManager(object):
         self.apps_collection = AppCollections.CustomApps
         self.apps_per_agent_collection = AppCollections.CustomAppsPerAgent
 
-    def get_apps(self, os_code, views):
-        apps_info = (
-            fetch_apps_data_by_os_code(
-                os_code, views,
-                collection=self.apps_collection
+    def get_apps(self, app_id=None, os_code=None, views=None):
+        if os_code and views and not app_id:
+            apps_info = (
+                fetch_apps_data_by_os_code(
+                    os_code, views,
+                    collection=self.apps_collection
+                )
             )
-        )
+        elif app_id and not os_code and not views:
+            apps_info = (
+                fetch_app_data(app_id, collection=self.apps_collection)
+            )
+
         return apps_info
 
     def get_agent_ids(self, os_code, views):
@@ -41,56 +48,55 @@ class CustomAppsManager(object):
         )
         return agent_ids
 
-def add_custom_app_to_agents(file_data, views=None,
-                             agent_id=None, app_id=None):
+    def add_apps_to_agents(self, file_data, views=None, agent_id=None,
+                           app_id=None):
 
-    if app_id and not agent_id:
-        app_info = (
-            fetch_app_data(
+        if app_id and not agent_id:
+            app_info = (
+                fetch_app_data(
                 app_id, collection=AppCollections.CustomApps
+                )
             )
-        )
 
-        agent_ids = (
-            fetch_agent_ids_in_views(
-                views=views, os_string=app_info[AgentKeys.OsCode]
+            agent_ids = (
+                self.get_agent_ids(app_info[AgentKeys.OsCode], views)
             )
-        )
-        if len(agent_ids) > 0:
-            for agentid in agent_ids:
+
+            if len(agent_ids) > 0:
+                for agentid in agent_ids:
+                    add_file_data(app_id, file_data, agent_id)
+                    agent_info_to_insert = (
+                        {
+                            CustomAppsPerAgentKey.AgentId: agentid,
+                            CustomAppsPerAgentKey.AppId: app_id,
+                            CustomAppsPerAgentKey.Status: CommonAppKeys.AVAILABLE,
+                            CustomAppsPerAgentKey.InstallDate: r.epoch_time(0.0)
+                        }
+                    )
+                    insert_app_data(
+                        agent_info_to_insert,
+                        collection=AppCollections.CustomAppsPerAgent
+                    )
+
+        elif agent_id and not app_id:
+            agent_info = fetch_agent(agent_id)
+            apps_info = fetch_apps_data_by_os_code(
+                agent_info[AgentKeys.OsCode], views,
+                collection=AppCollections.CustomApps
+            )
+
+            for app_info in apps_info:
+                app_id = app_info.get(CustomAppsKey.AppId)
+                file_data = fetch_file_data(app_id)
                 add_file_data(app_id, file_data, agent_id)
-                agent_info_to_insert = (
-                    {
-                        CustomAppsPerAgentKey.AgentId: agentid,
-                        CustomAppsPerAgentKey.AppId: app_id,
-                        CustomAppsPerAgentKey.Status: CommonAppKeys.AVAILABLE,
-                        CustomAppsPerAgentKey.InstallDate: r.epoch_time(0.0)
-                    }
-                )
+
+                agent_info_to_insert = {
+                    CustomAppsPerAgentKey.AgentId: agent_id,
+                    CustomAppsPerAgentKey.AppId: app_id,
+                    CustomAppsPerAgentKey.Status: CommonAppKeys.AVAILABLE,
+                    CustomAppsPerAgentKey.InstallDate: r.epoch_time(0.0)
+                }
+
                 insert_app_data(
-                    agent_info_to_insert,
-                    collection=AppCollections.CustomAppsPerAgent
+                    agent_info_to_insert, collection=AppCollections.CustomAppsPerAgent
                 )
-
-    if agent_id and not app_id:
-        agent_info = fetch_agent(agent_id)
-        apps_info = fetch_apps_data_by_os_code(
-            agent_info[AgentKeys.OsCode], views,
-            collection=AppCollections.CustomApps
-        )
-
-        for app_info in apps_info:
-            app_id = app_info.get(CustomAppsKey.AppId)
-            file_data = fetch_file_data(app_id)
-            add_file_data(app_id, file_data, agent_id)
-
-            agent_info_to_insert = {
-                CustomAppsPerAgentKey.AgentId: agent_id,
-                CustomAppsPerAgentKey.AppId: app_id,
-                CustomAppsPerAgentKey.Status: CommonAppKeys.AVAILABLE,
-                CustomAppsPerAgentKey.InstallDate: r.epoch_time(0.0)
-            }
-
-            insert_app_data(
-                agent_info_to_insert, collection=AppCollections.CustomAppsPerAgent
-            )
