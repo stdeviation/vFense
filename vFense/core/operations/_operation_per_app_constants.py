dc16c0e21749f766060e0a51c4deace8882b52c3
Allen Sanabria <asanabria@linuxdynasty.org>
2014-10-06 09:09:25 -0400

    Working on operation classes

---
+++ vFense/core/operations/_operation_per_app_constants.py
@@ -0,0 +1,16 @@
+class OperPerAppDefaults():
+    @staticmethod
+    def results_received_time():
+        return 0
+
+    @staticmethod
+    def errors():
+        return list()
+
+    @staticmethod
+    def apps_removed():
+        return list()
+
+    @staticmethod
+    def results():
+        return list()
