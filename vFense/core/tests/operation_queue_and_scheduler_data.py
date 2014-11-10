ccd878c0fc7c2c4efd506e816427b8ad93e0046f
Allen Sanabria <asanabria@linuxdynasty.org>
2014-11-10 07:54:37 -0500

    operation test data

---
+++ vFense/core/tests/operation_queue_and_scheduler_data.py
@@ -0,0 +1,40 @@
+stat_data = {
+    'cpu': {
+        'iowait': 0.22,
+        'idle': 94.5,
+        'user': 3.6,
+        'system': 1.64
+    },
+    'file_system': [
+        {
+            'used_percent': 8.0,
+            'used': 17408644.0,
+            'name': '/dev/mapper/ubuntu--vg-root',
+            'mount': '/',
+            'free_percent': 92.0,
+            'free': 200249136.0
+        },
+        {
+            'used_percent': 0.0,
+            'used': 4.0,
+            'name': 'udev',
+            'mount': '/dev',
+            'free_percent': 100.0,
+            'free': 8159784.0
+        },
+        {
+            'used_percent': 29.23,
+            'used': 66810.0,
+            'name': '/dev/sda1',
+            'mount': '/boot',
+            'free_percent': 70.77,
+            'free': 161721.0
+        }
+    ],
+    'memory': {
+        'used_percent': 12.74,
+        'used': 2081388.0,
+        'free_percent': 87.26,
+        'free': 14260528.0
+    }
+}
